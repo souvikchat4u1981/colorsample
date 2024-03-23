680a965e43f2f1f48d6c04eec499410301426616
souvikchat4u1981 <souvik.chatterjee1981@gmail.com>
2024-03-23 08:05:18 +0530

    Source code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import colors from "./colors.json";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 function App() {
+  var height = 100 / colors.length - 5 + "vh";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid mt-3">
+      <div className="row">
+        {colors &&
+          colors.map((colorGroup, index) => {
+            return (
+              <div className="col-sm-6">
+                <div key={index} className="card mb-2">
+                  <div className="card-header">{colorGroup.groupName}</div>
+                  <div className="card-body">
+                    <div
+                      className="d-flex justify-content-around flex-wrap"
+                      style={{ height: "auto" }}
+                    >
+                      {colorGroup.colors.map((c, index) => {
+                        return (
+                          <CopyToClipboard text={c.color}>
+                            <div
+                              className="color-sample text-center p-4 mb-2 me-1"
+                              style={{ background: c.color, cursor: "pointer" }}
+                              title={c.color}
+                            >
+                              {c.name}
+                            </div>
+                          </CopyToClipboard>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
